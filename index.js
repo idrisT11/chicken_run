be44d41f79e53ded3a9cefbece4c8b3f1a6a25b7
idris <64688223+idrisT11@users.noreply.github.com>
2022-06-19 19:52:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "r00TR00t",           // <<<< A modifier
+    password: "",           // <<<< A modifier
     database: 'chicken_run'
 })
 
